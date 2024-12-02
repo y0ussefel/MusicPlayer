2c50c87759e8c5bea08167ee73dd08aebb286483
y0ussefel <youxfenari@gmail.com>
2024-12-02 17:45:23 +0100

    modify

--- MusicPlayer_Project/app/src/main/java/com/example/musicplayer_project/MainActivity.kt
+++ MusicPlayer_Project/app/src/main/java/com/example/musicplayer_project/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.player)
 
     }
 }
